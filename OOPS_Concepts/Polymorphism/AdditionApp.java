8f7820c542d3c75426c69dc75e2c0ce61852d21d
HARISH KUMBHAR <118440588+KodWithHarish@users.noreply.github.com>
2023-07-14 21:27:16 +0530

    Delete OOPS_Concepts directory

--- OOPS_Concepts/Polymorphism/AdditionApp.java
+++
@@ -1,7 +0,0 @@
-public class AdditionApp {
-    public static void main(String[] args) {
-        Addition add = new Addition();
-        System.out.println(add.sum(10, 10));
-        System.out.println(add.sum(10, 20, 30));
-    }
-}
