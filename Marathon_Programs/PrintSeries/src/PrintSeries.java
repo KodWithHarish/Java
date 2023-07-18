e8f351cad6acb0581f9203dec0b9413442971aad
HARISH KUMBHAR <118440588+KodWithHarish@users.noreply.github.com>
2023-07-18 19:33:52 +0530

    Java Logical Based Practice Questions

---
+++ Marathon_Programs/PrintSeries/src/PrintSeries.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class PrintSeries {
+	public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Number to Generate Series : ");
+        int n=sc.nextInt();
+        for(int i=1;i<=n;i++)
+        if(n % 2 != 0) {
+            System.out.print((2 * i - 1)+" ");
+        }
+        else{
+                int a=(2 * (i - 1) -1);
+                if(a>0) {
+                System.out.print(a+" ");
+            }
+        }
+        sc.close();
+    }
+}
