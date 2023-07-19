a794384599b80b23204af33a200599adfa5fe2e4
HARISH KUMBHAR <118440588+KodWithHarish@users.noreply.github.com>
2023-07-20 00:34:47 +0530

    Math Class Methods
    Example : round(), ceil(), floor(), sqrt(), cbrt(), pow(), etc

---
+++ Assignment/MathMethods/src/MathMethods.java
@@ -0,0 +1,13 @@
+class MathMethods{
+    public static void main(String[] args) {
+    	System.out.println("Math Methods: ");
+        System.out.println("abs(): "+Math.abs(-5.5));
+        System.out.println("sqrt(): "+Math.sqrt(9));
+        System.out.println("cbrt(): "+Math.cbrt(8));
+        System.out.println("round(): "+Math.round(11.6));
+        System.out.println("ceil(): "+Math.ceil(8.1));
+        System.out.println("floor(): "+Math.floor(14.8));
+        System.out.println("pow(): "+Math.pow(3,2));
+        System.out.println("random(): "+Math.random());
+	}
+}
