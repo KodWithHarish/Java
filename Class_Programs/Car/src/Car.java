3e91698f91472da41ae20ede9ddc53cf77af1162
HARISH KUMBHAR <118440588+KodWithHarish@users.noreply.github.com>
2023-07-21 23:52:29 +0530

    Daily Class Programs

---
+++ Class_Programs/Car/src/Car.java
@@ -0,0 +1,18 @@
+
+public class Car {
+	String name;
+	int id;
+	double price;
+
+	void drive() {
+	System.out.println("Car is driving");
+	}
+	void run() {
+	System.out.println("car is run");
+	}
+	Car(String a,int b,double c){
+	this.name=a;
+	this.id=b;
+	this.price=c;
+	}
+}
